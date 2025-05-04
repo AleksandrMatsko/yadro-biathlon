95cd7a9ae984ce73acf619ef239d3ebd60299e66
Aleksandr Matsko <90016901+AleksandrMatsko@users.noreply.github.com>
2025-05-04 15:04:06 +0700

    feat/config-reading (#1)

--- cmd/biathlon-reporter/main.go
+++ cmd/biathlon-reporter/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/AleksandrMatsko/yadro-biathlon/internal/config"
+)
+
+var (
+	configFileNameFlag = flag.String("config", "", "Path to configuration file")
+	printConfigFlag    = flag.Bool("print-config", false, "Print current config to stdout")
+)
 
 func main() {
-	fmt.Println("hello world")
+	flag.Parse()
+
+	if configFileNameFlag == nil || *configFileNameFlag == "" {
+		fmt.Fprint(os.Stderr, "Config file required, provide it with --config option\n")
+		os.Exit(1)
+	}
+
+	conf := config.BiathlonCompetition{}
+	err := config.Read(*configFileNameFlag, &conf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+
+	if *printConfigFlag {
+		config.Print(conf)
+	}
 }
